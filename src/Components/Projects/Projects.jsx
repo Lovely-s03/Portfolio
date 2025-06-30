10cc7440a01452a18e16c83ca0e9ffb5f8bbe05b
Lovely-s03 <supriyalovely0306@gmail.com>
2025-06-30 19:31:04 +0530

    changes

--- src/Components/Projects/Projects.jsx
+++ src/Components/Projects/Projects.jsx
@@ -5,7 +5,7 @@ const Projects = () => {
   return (
     <div id="Projects" className="p-10 md:p-24 text-white ">
       <h1 className="text-2xl md:text-4xl text-white text-center font-bold">Projects</h1>
-      <div className="py-12 px-8 grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3  gap-8">
+      <div className="py-12 xl:px-8 grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3  gap-8">
      
         <ProjectCard
           title="Ecommerce Website"
